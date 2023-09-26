7549169208e48f93e0a483689d1b78da8ba70995
Yukio-Nishida <142161225+Yukio-Nishida@users.noreply.github.com>
2023-09-26 21:52:38 +0900

    Add files via upload

---
+++ kadai_005/kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//BMIの変数を宣言
+let BMI;
+
+//BMIの変数を代入
+BMI = ('体格指数');
+
+//体重を指定
+weight = (68);
+
+//身長の変数を宣言
+let height;
+
+//身長を指定
+height = (1.7);
+
+// BMIを求める計算を実行
+console.log(BMI + 'は' + (weight / (height * height)) + 'です。'); 
